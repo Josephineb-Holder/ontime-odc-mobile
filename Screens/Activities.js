77dffac5894da29d2ce593ad20be09eb2b391fdf
Josephineb-Holder <89983188+Josephineb-Holder@users.noreply.github.com>
2023-09-18 12:10:05 +0000

    activities first commit

--- Screens/Activities.js
+++ Screens/Activities.js
@@ -1,24 +1,33 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import COLORS from "../Constants/Colors";
 
 const Activities = () => {
   return (
+    <>
     <View
       style={{
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
         backgroundColor: COLORS.gray,
       }}
     >
-      <Text style={{ color: COLORS.primary, fontSize: 24, fontWeight: "bold" }}>
-        Activities
-      </Text>
+      <View style={{ color: COLORS.primary, fontSize: 14, marginHorizontal:25, marginVertical: 10,padding:10, borderRadius: 15, borderWidth: 2, borderColor: 'red'}}>
+       <Text style={styles.eventText}>Attending: React Native Class</Text> 
+        <Text style={styles.eventText}>Subject: Coding School</Text>
+       <Text style={styles.eventText}>Purpose: Training</Text> 
+      </View>
     </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  eventText:{
+    paddingLeft: 15
+  }
+})
+
 
 export default Activities;
