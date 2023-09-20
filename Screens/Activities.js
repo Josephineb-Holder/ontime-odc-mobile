948e799d343d7ee3ea539730805810725b298ca0
Josephineb-Holder <89983188+Josephineb-Holder@users.noreply.github.com>
2023-09-20 04:51:30 -0400

    activities second commit

--- Screens/Activities.js
+++ Screens/Activities.js
@@ -2,32 +2,56 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Calendar } from "react-native-calendars";
 import COLORS from "../Constants/Colors";
 
 const Activities = () => {
   return (
     <>
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: COLORS.gray,
-      }}
-    >
-      <View style={{ color: COLORS.primary, fontSize: 14, marginHorizontal:25, marginVertical: 10,padding:10, borderRadius: 15, borderWidth: 2, borderColor: 'red'}}>
-       <Text style={styles.eventText}>Attending: React Native Class</Text> 
-        <Text style={styles.eventText}>Subject: Coding School</Text>
-       <Text style={styles.eventText}>Purpose: Training</Text> 
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.gray,
+        }}
+      >
+        <View style={styles.viewStyle}>
+          <Text style={styles.eventText}>Attending: React Native Class</Text>
+          <Text style={styles.eventText}>Subject: Coding School</Text>
+          <Text style={styles.eventText}>Purpose: Training</Text>
+        </View>
+        <Calendar style={styles.calendar} />
+        <View style={styles.viewStyle}>
+          <Text>August 2023</Text>
+        </View>
       </View>
-    </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  eventText:{
-    paddingLeft: 15
-  }
-})
-
+  viewStyle: {
+    color: COLORS.primary,
+    fontSize: 14,
+    marginHorizontal: "8%",
+    marginVertical: "3%",
+    padding: "3%",
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  eventText: {
+    paddingLeft: 15,
+  },
+  calendar: {
+    borderWidth: 2,
+    borderColor: "red",
+    marginTop: "20%",
+    marginHorizontal: "6%",
+    marginVertical: "6%",
+    padding: "6%",
+    borderRadius: 15,
+    backgroundColor: "black",
+  },
+});
 
 export default Activities;
